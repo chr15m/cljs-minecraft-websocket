f8aed1a4a7b29547018f5804dc40172c5c09962c
Chris McCormick <chris@mccormick.cx>
2024-11-08 16:23:51 +0000

    Got command sending working.

--- minerepl.cljs
+++ minerepl.cljs
@@ -11,6 +11,27 @@
 
 (print (count @connections) "connections")
 
+(defn socket-send [socket packet]
+  (->> packet
+       clj->js
+       js/JSON.stringify
+       (.send socket)))
+
+(defn command-packet [command]
+  {:header {:version 1
+            :requestId (str (random-uuid))
+            :messagePurpose "commandRequest"
+            :messageType "commandRequest"}
+   :body {:version 1
+          :commandLine command
+          :origin {:type "player"}}})
+
+(defn send-command [command]
+  (doseq [socket @connections]
+    (socket-send socket (command-packet command))))
+
+; (send-command "setblock ~2 ~2 ~2 emerald_block")
+
 (defn subscribe-to-events [socket event-name]
   (->>
     (j/lit
